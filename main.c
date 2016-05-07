6545bffaeb52813e5bb51ce9ac36f45f172b341b
Pecli <Pecli@Pecli-PC>
2016-05-07 16:40:19 +0200

    Merge origin/master
    Conflicts:
    	nbproject/configurations.xml
    
    avancé sur la fonction main.

--- main.c
+++ main.c
@@ -45,8 +45,31 @@ unsigned char conversionMagnitude(unsigned char v) {
  */
 static void hardwareInitialise() {
     
+    ANSELA = 0X00;
+    ANSELB = 0X01;
+    ANSELC = 0X00;
+   
+    TRISC = 0x00; //Configure le port C comme sortie digitale
+    
     // Prépare Temporisateur 2 pour PWM (compte jusqu'à 255 en 1ms):
-    // À faire...
+    
+    T2CONbits.TMR0ON = 1; //active le timer 0.
+    T2CONbits.T0CS = 0; //On utilise Fosc/4 comme source
+    T2CONbits.PSA = 1; //Pas de diviseur de fréquence
+    
+    //Etablit le compteur du timer 0 pour un débordement 
+    
+    TMR0H = 0xFC; //TMR0 =655036-[(255*4)/1E6] = 64556 = FC2C [exadécimal] 
+    TMR0L = 0x2C; 
+
+    /* Compte les interruptions du temporisateur pour mesurer les secondes */
+
+   
+    
+    /*Routine des interruptions: Détecte de quelle interruption il s'agit puis 
+     appelle la fonction correspondante*/
+    
+    
     
     // Configure PWM 1 pour émettre un signal de 1KHz:
     // À faire...
@@ -55,22 +78,34 @@ static void hardwareInitialise() {
     // À faire...
     
     // Active le module de conversion A/D:
-    // À faire...
+    
+    ADCON0bits.ADON = 1; //allume le module A/D
+    ADCON0bits.CHS = 9; //branche le convertisseur sur AN9
+    ADCON2bits.ADFM = 1; //les 8 bits les moins signifiants sont sur ADRESL
+    ADCON2bits.ACQT = 2; //temps d'acquisiton à 4 TAD
+    ADCON2bits.ADCS = 0; // À 1 MHz, le TAD est à 2 us
 
     // Active les interruptions générales:
-    // À faire...
+    
+     RCONbits.IPEN = 0; //Désactive le mode Haute / Basse priorité.
+    INTCONbits.GIEH = 0; //Désactive les interruptions hautes priorité.
+    INTCONbits.GIEL = 1; //Active les interruptions basses priorité.
+    
+    INTCONbits.TMR0IE = 1; //Active les interruptions timer 0.
+    PIE1bits.ADIE = 1; //Active les interruptions A/D.
+    IPR1bits.ADIP = 1; //Interr. A/D sont de haute priorité.
 }
 
 /**
  * Point d'entrée des interruptions.
  */
 void low_priority interrupt interruptionsBassePriorite() {
-    if (PIR1bits.TMR2IF) {
+    if (PIR1bits.TMR2IF) { //lance une conversion
         PIR1bits.TMR2IF = 0;
-        ADCON0bits.GO = 1;
+        ADCON0bits.GO = 1; 
     }
     
-    if (PIR1bits.ADIF) {
+    if (PIR1bits.ADIF) { //Commande le moteur
         PIR1bits.ADIF = 0;
         PORTC = conversionDirection(ADRESH);
         CCPR1L = conversionMagnitude(ADRESH);
